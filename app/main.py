e4d337293d3e1717147d9d63795ad82474aff50e
Your Name <you@example.com>
2025-04-04 18:36:03 +0700

    Update  with new configurations

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ load_dotenv()
 
 app = FastAPI()
 
-ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS")
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
